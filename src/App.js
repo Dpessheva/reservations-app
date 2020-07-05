ba05ed5677c7db3f58cd2f24a89e167cdb1279a9
Dpessheva <dpessheva@gmail.com>
2020-07-05 11:30:15 +0200

    added routes

--- src/App.js
+++ src/App.js
@@ -1,18 +1,22 @@
-import React from 'react';
-import './App.css';
-import Home from './components/Home';
-import Rooms from './components/Rooms';
-import SingleRoom from './components/SingleRoom';
-import ErrorPage from './components/ErrorPage';
-
-function App() {
+import React from "react";
+import "./App.css";
+import Home from "./components/Home";
+import Rooms from "./components/Rooms";
+import SingleRoom from "./components/SingleRoom";
+import ErrorPage from "./components/ErrorPage";
+import { Route, Switch, Router } from "react-router-dom";
+import Navbar from "./components/NavBar";
+export const App = ()=> {
   return (
     <>
-      <Home></Home>
-      <Rooms></Rooms>
-      <SingleRoom></SingleRoom>
-      <ErrorPage></ErrorPage>
-   </>
+      <Navbar/>
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/rooms/" component={Rooms} />
+        <Route exact path="/rooms/:slug" component={SingleRoom} />
+        <Route component={ErrorPage} />
+      </Switch>
+    </>
   );
 }
 
